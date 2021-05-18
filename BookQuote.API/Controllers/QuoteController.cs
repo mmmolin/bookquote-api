663cfdfddc460665fd0a06e1ec4d958a74224a74
Martin <mmolin@telia.com>
2021-05-19 00:01:49 +0200

    Added Controller Folder, changes to Startup

---
+++ BookQuote.API/Controllers/QuoteController.cs
@@ -0,0 +1,36 @@
+﻿using BookQuote.Core.Entities;
+using BookQuote.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookQuote.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+        private IQuoteRepository quoteRepository;
+
+        public QuoteController(IQuoteRepository quoteRepository)
+        {
+            this.quoteRepository = quoteRepository;
+        }
+
+        [HttpPost]
+        public void PostQuote(Quote quote)
+        {
+            quoteRepository.Add(quote);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Quote>> GetAll()
+        {
+            return quoteRepository.GetAll();
+        }
+        //[HttpPut]
+    }
+}
